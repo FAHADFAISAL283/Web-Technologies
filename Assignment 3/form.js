5a90337d3012634debcceaf557ac221352a6adc7
FAHADFAISAL283 <fahadfaisal283@gmail.com>
2024-03-08 20:29:21 +0500

    Form

--- Assignment 3/form.js
+++ Assignment 3/form.js
@@ -5,3 +5,18 @@ function openwid(){
     window.open("form.html")
 }
 
+function validateForm() {
+    var email = document.getElementById("exampleInputEmail1").value;
+    var password = document.getElementById("exampleInputPassword1").value;
+
+    if (email.trim() === '' || password.trim() === '') {
+      alert("Please fill in all fields");
+      return false;
+    }
+
+    return true;
+  }
+
+
+
+
