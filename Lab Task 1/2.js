b35cfde60885e98491704b40ca39d922456b3c92
FAHADFAISAL283 <fahadfaisal283@gmail.com>
2024-05-05 16:14:19 +0500

    Shop

---
+++ Lab Task 1/2.js
@@ -0,0 +1,19 @@
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+
+btn1.onclick = openwid;
+btn2.onclick = openwid1;
+btn3.onclick = openwid2;
+
+
+function openwid() {
+    window.open("shopbat.html");
+}
+
+function openwid1() {
+    window.open("shopfootball.html");
+}
+function openwid2() {
+    window.open("shopracket.html");
+}
