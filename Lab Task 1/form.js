99e6899c0ef2ec7e14f2ff224d3e4e88e7bf1661
FAHADFAISAL283 <fahadfaisal283@gmail.com>
2024-05-24 19:37:58 +0500

    database added

--- Lab Task 1/form.js
+++ Lab Task 1/form.js
@@ -18,31 +18,6 @@ function validateForm() {
     return true;
   }
 
-  const login=document.querySelector("#login");
- 
-
-  home=document.querySelector(".home");
-  form_container=document.querySelector(".form-container");
- form_close=document.querySelector(".form-close");
-  login_link=document.querySelector("#login-link");
-  Signup_link=document.querySelector("#signup-link");
-  ;
-  pwShowHide=document.querySelectorAll(".pw");
- 
-  login.addEventListener("click",()=>home.classList.add("show"));
-  
-  
- 
-  form_close.addEventListener("click",()=>home.classList.remove("show"));
- 
-  Signup_link.addEventListener("click",(e)=>{
-   e.preventDefault();
-   form_container.classList.add("active");
-  })
-  login_link.addEventListener("click",(e)=>{
-   e.preventDefault();
-   form_container.classList.remove("active");
-  })
 
   function validateLoginForm(){
     var email = document.getElementById("exampleInputName").value;
